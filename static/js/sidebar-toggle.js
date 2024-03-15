ded355a5ac6e083aa52ea066a618586a72613680
Lucas Oskorep <lucas.oskorep@gmail.com>
2024-03-15 13:00:30 -0400

    feat: adding fun animations and a mobile bar header

---
+++ static/js/sidebar-toggle.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $(".sidebar-toggle").click(function() {
+    $(".sidebar").toggleClass("active");
+    $(".sidebar-canvas").toggleClass("active");
+
+  });
+
+  $("a[href*=\\#]").on('click', function(event) {
+    if (this.pathname === window.location.pathname) {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 600, function() {
+        window.location.hash = hash;
+      });
+    }
+  });
+});
